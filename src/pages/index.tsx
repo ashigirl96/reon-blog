47ac0e46158b35f16c4778fed806d469abfe6065
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2022-04-12 23:39:45 +0900

    Fix height

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,21 +10,29 @@ const Home: NextPage = () => {
       title="1e0nhard's Blog"
       description="top"
     >
-      <div className="flex flex-col md:flex-row max-h-full">
-        <div className="hero min-h-[75vh]">
-          {/*背景をぼかしてる */}
-          <div className="hero-overlay bg-opacity-80" />
-          {/* heroの中身 */}
-          <div className="hero-content text-center text-neutral-content">
-            <div className="max-w-md">
-              <h1 className="mb-5 text-5xl font-bold">1e0nhard's Blog</h1>
-              <p className="mb-5">
-                We learn what doesn't kill us makes us stronger.
-              </p>
-            </div>
-          </div>
+      <div className="hero min-h-[74vh]">
+        <div className="">
+          <h1 className="mb-5 text-5xl font-bold">1e0nhard's Blog</h1>
+          <p className="mb-5">
+            We learn what doesn't kill us makes us stronger.
+          </p>
         </div>
       </div>
+      {/*<div className="flex flex-col md:flex-row flex-grow">*/}
+      {/*  /!*<div className="hero max-h-screen">*!/*/}
+      {/*  /!*  /!*背景をぼかしてる *!/*!/*/}
+      {/*  /!*  <div className="hero-overlay bg-opacity-80" />*!/*/}
+      {/*  /!*  /!* heroの中身 *!/*!/*/}
+      {/*  /!*  <div className="hero-content text-center text-neutral-content">*!/*/}
+      {/*  /!*    <div className="max-w-md">*!/*/}
+      {/*  /!*      <h1 className="mb-5 text-5xl font-bold">1e0nhard's Blog</h1>*!/*/}
+      {/*  /!*      <p className="mb-5">*!/*/}
+      {/*  /!*        We learn what doesn't kill us makes us stronger.*!/*/}
+      {/*  /!*      </p>*!/*/}
+      {/*  /!*    </div>*!/*/}
+      {/*  /!*  </div>*!/*/}
+      {/*  /!*</div>*!/*/}
+      {/*</div>*/}
     </Page>
   )
 }
